0bcaf75a1a67718fe74ca77d6434f9f254b3fcb8
StamLord <42681607+StamLord@users.noreply.github.com>
2025-03-26 13:27:32 +0200

    Organized resources

--- src/Canvas.svelte
+++ src/Canvas.svelte
@@ -177,7 +177,7 @@
 
   onMount(() => {
         const iframe = document.getElementById("game-iframe");
-        iframe.src = "../static/LevelEditorWebApp.html";
+        iframe.src = "/level-editor/static/LevelEditorWebApp.html";
         updateFloorButtons();
     });
 
