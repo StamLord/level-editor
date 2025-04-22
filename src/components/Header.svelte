9a25d4940ef08ab559292894ed1362b70bf2f038
StamLord <42681607+StamLord@users.noreply.github.com>
2025-04-22 21:09:35 +0300

    Help & Popup component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,12 @@
 <script>
   import LinkButton from "./LinkButton.svelte";
+  import Popup from "./Popup.svelte";
+
+  let isPopupVisible = false;
+
+  function showPopup() {
+    isPopupVisible = true;
+  }
 </script>
 
 <header>
@@ -12,10 +19,39 @@
       <div class="nav-right">
         <LinkButton text="☕ Buy Me a Coffee" link="https://buymeacoffee.com/philip.grach"/>
         <LinkButton text="💡 Leave a Suggestion" link="https://docs.google.com/forms/d/e/1FAIpQLSeNaHLzdSS5fEi2IAAc5a8ENb0xir8lOD7Axro6NIfYB0HrAA/viewform?usp=sharing"/>
-        <button class="nav-button">Help</button>
+        <button class="nav-button" on:click={showPopup}>❓ Help</button>
       </div>
     </nav>
 </header>
+
+<Popup bind:visible={isPopupVisible}>
+  <h3>What is <big class="logo" style="margin:0.2rem;">LevelForge</big>?</h3>
+  <p>
+    LevelForge is a web-app for quickly designing and prototyping game levels.
+    It’s lightweight, fast, and built to streamline your creative process.
+  </p>
+
+  <h3>Who is it for?</h3>
+  <p>
+    LevelForge is made for game developers and level designers who want to iterate quickly, refine their layouts, and share ideas with others.<br><br>
+    While it includes an embedded Godot engine preview and supports exporting to `.tscn` format, LevelForge aims to be an engine-agnostic tool for level designers.
+  </p>
+
+  <h3>How do I use it?</h3>
+  <p>
+    Use the <strong class="btn">Q</strong><strong class="btn">W</strong><strong class="btn">E</strong><strong class="btn">R</strong><strong class="btn">T</strong><strong class="btn">S</strong> tools above the canvas to start drawing your level. <br><br>
+    Each tool has its own hotkey, so you can switch between them without moving your cursor away from the canvas.<br><br>
+
+    Add floors with the <strong class="btn">+</strong> button on the right side of the canvas, and switch between floors using the displayed buttons or your number keys: <strong>1–9</strong>.<br><br>
+
+    The <strong class="red-btn">➤</strong> button launches an embedded Godot game, allowing you to playtest your level.
+    When you're done testing, use the same button (now <strong class="red-btn">⏸︎</strong>) to return to the editor and continue iterating on your design.<br><br>
+
+    Use the <strong class="btn">Export</strong> and <strong class="btn">Import</strong> buttons to save or load your designs as `.json` files.<br><br>
+
+    To continue development in Godot, use the <strong class="btn">Export to Godot</strong> button to download a `.tscn` scene file that you can load in your own Godot game.<br><br>
+  </p>
+</Popup>
 
 <style>
   header {
@@ -84,5 +120,23 @@
     background-color: var(--accent-color);
     cursor: pointer;
     transition: background-color 0.25s;
+  }
+
+  .btn {
+    background: var(--button-bg); 
+    padding: 0.2rem 0.5rem; 
+    border-radius: 4px;
+  }
+
+  .red-btn {
+    background: var(--red-color); 
+    margin: 0.2rem;
+    padding: 0.2rem 0.5rem; 
+    border-radius: 4px;
+  }
+  
+  h3 {
+    padding-bottom: 0.3em;
+    border-bottom: 2px solid var(--divider-color);
   }
 </style>
---
+++ src/components/Popup.svelte
@@ -0,0 +1,39 @@
+<script>
+    export let visible = false;
+</script>
+
+{#if visible}
+    <div class="popup-bg" on:click={() => {
+        if(visible){visible = false;}}}>
+        <div class="popup" on:click|stopPropagation>
+            <slot></slot>
+        </div>
+    </div>
+{/if}
+
+<style>
+    .popup-bg {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        backdrop-filter: blur(4px);
+        align-content: center;
+        z-index: 1000;
+    }
+
+    .popup {
+        background: var(--panel-color);
+        padding: 0.5rem 2rem;
+        overflow-y: auto;
+        margin: auto;
+        border-radius: 12px;
+        max-width: 500px;
+        height: 75%;
+        box-shadow: 0 10px 30px rgba(0, 0, 0, 0.3);
+        text-align: left;
+        z-index: 2000;
+    }
+</style>
